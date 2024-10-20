3eda0f2847a83486de5bc1124eb502ee6877f79b
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-20 20:10:17 +0200

    feat: add header and nav with grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-
+import React from 'react';
 import './App.css'
-import { Container } from '@mui/material'
+import { Container, Grid2, Typography,  } from '@mui/material'
 
 
 function App() {
@@ -8,10 +8,29 @@ function App() {
 
   return (
     <>
-    <Container maxWidth="sm" className='home-container' >
-     <div>
-      salam
-     </div>
+
+    <Container maxWidth="sm"  >
+      <header className='home-header'>
+        <Typography variant="h2" color="error" gutterBottom>
+        دیوار
+
+        </Typography>
+        <nav className='home-nav'>
+        <Grid2 container spacing={4 } >
+
+          <Grid2><a href='#'>ثبت آگهی</a></Grid2>
+          <Grid2><a href='#'>درباره دیوار</a></Grid2>
+          <Grid2><a href='#'>دریافت برنامه</a></Grid2>
+          <Grid2><a href='#'>اتاق خبر</a> </Grid2>
+          <Grid2><a href='#'>پشتیبانی</a></Grid2>
+
+        </Grid2>
+
+        </nav>
+
+      </header>
+     
+    
 
     </Container>
 
