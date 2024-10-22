a017f394900a7d97a2771294071447fcf0ed2d0a
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-22 15:21:52 +0200

    refactor: extract HomePage component

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Box, Container, Grid2, Typography, TextField } from "@mui/material";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+import Logo from "./Logo";
+import Footer from "./Footer";
+const cities = [
+    "تهران",
+    "مشهد",
+    "کرج",
+    "شیراز",
+    "اصفهان",
+    "اهواز",
+    "تبریز",
+    "کرمانشاه",
+    "قم",
+    "رشت",
+  ];
+
+export default function HomePage() {
+    return (
+        <>
+          <Container maxWidth="sm">
+            <header className="home-header">
+              <Logo />
+              <nav className="home-nav">
+                <Grid2 container spacing={6}>
+                  <Grid2>
+                    <a href="#">ثبت آگهی</a>
+                  </Grid2>
+                  <Grid2>
+                    <a href="#">درباره ما</a>
+                  </Grid2>
+                  <Grid2>
+                    <a href="#">دریافت برنامه</a>
+                  </Grid2>
+                  <Grid2>
+                    <a href="#">اتاق خبر</a>{" "}
+                  </Grid2>
+                  <Grid2>
+                    <a href="#">پشتیبانی</a>
+                  </Grid2>
+                </Grid2>
+              </nav>
+            </header>
+            <Box textAlign="center" my={4}>
+              <Typography className="home-desc" variant="body1" gutterBottom>
+                بزرگترین ، ﭘﺎﯾﮕﺎه ﺧﺮﯾﺪ و ﻓﺮوش ﺑﯽ‌واﺳﻄﻪ‌!
+              </Typography>
+              <Typography className="home-desc" variant="body1" gutterBottom>
+                اﮔﻪ دﻧﺒﺎل ﭼﯿﺰی ﻫﺴﺘﯽ، ﺷﻬﺮت رو اﻧﺘﺨﺎب ﮐﻦ و ﺗﻮ دﺳﺘﻪ‌ﺑﻨﺪی‌ﻫﺎ ﺑﻪ دﻧﺒﺎﻟﺶ
+                ﺑﮕﺮد. اﮔﺮ ﻫﻢ ﻣﯽ‌ﺧﻮای ﭼﯿﺰی ﺑﻔﺮوﺷﯽ، ﭼﻨﺪ ﺗﺎ ﻋﮑﺲ ﺧﻮب ازش ﺑﮕﯿﺮ و آﮔﻬﯿﺖ رو
+                ﺑﭽﺴﺒﻮن ﺑﻪ ما.
+              </Typography>
+            </Box>
+            <Box my={8}>
+              <TextField
+                placeholder=" شهر سرچ کن ! "
+                type="search"
+                fullWidth
+                className="search-input"
+                slotProps={{
+                  input: {
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+              />
+            </Box>
+            <Box my={4} className="home-boxCity">
+              <Typography variant="h6" className="home-desc" my={4}>
+                شهرهای پربازدید
+              </Typography>
+              <Grid2
+                container
+                justifyContent="center"
+                justifyItems="center"
+                spacing={10}
+              >
+                {cities.map((city) => (
+                  <Grid2 key={city}>
+                    <Typography className="home-cityItem">{city}</Typography>
+                  </Grid2>
+                ))}
+              </Grid2>
+            </Box>
+            <footer>
+              <Footer />
+            </footer>
+          </Container>
+        </>
+      );
+}
