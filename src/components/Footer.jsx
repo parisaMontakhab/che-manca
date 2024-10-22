ed92d8fa972198c3853f3aedb151114c13b9d5cd
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-22 15:15:51 +0200

    refactor: use pritier

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import { Box, Container, Grid2,  } from '@mui/material';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TelegramIcon from '@mui/icons-material/Telegram';
+import React from "react";
+import { Box, Container, Grid2 } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TelegramIcon from "@mui/icons-material/Telegram";
 
 export default function Footer() {
   return (
-    <Box className='footer-box'>
-         <Grid2 container spacing={4} justifyContent='center' justifyItems='center'>
-        <Grid2><a href='#'>
-          <LinkedInIcon className='social-icon'/>
-          </a></Grid2>
-          <Grid2><a href='#'>
-          <TelegramIcon className='social-icon'/>
-          </a></Grid2>
-          <Grid2><a href='#'>
-          <InstagramIcon className='social-icon'/>
-          </a></Grid2>
+    <Box className="footer-box">
+      <Grid2
+        container
+        spacing={4}
+        justifyContent="center"
+        justifyItems="center"
+      >
+        <Grid2>
+          <a href="#">
+            <LinkedInIcon className="social-icon" />
+          </a>
+        </Grid2>
+        <Grid2>
+          <a href="#">
+            <TelegramIcon className="social-icon" />
+          </a>
+        </Grid2>
+        <Grid2>
+          <a href="#">
+            <InstagramIcon className="social-icon" />
+          </a>
+        </Grid2>
       </Grid2>
     </Box>
-  )
+  );
 }
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from '../assets/img/logo.png';
-import {  Box} from '@mui/material';
+import React from "react";
+import logo from "../assets/img/logo.png";
+import { Box } from "@mui/material";
 
 export default function Logo() {
   return (
-    <Box display='flex' justifyContent='center' alignItems='center'p={2}>
-      <img src={logo} alt='chemanca-logo'/>
-     </Box>
-  )
+    <Box display="flex" justifyContent="center" alignItems="center" p={2}>
+      <img src={logo} alt="chemanca-logo" />
+    </Box>
+  );
 }
