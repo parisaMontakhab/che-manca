8679229e55da3044b682dba97f6e677f2c1f82be
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-24 16:17:35 +0200

    feat: add autocomplete about search countries/province/cities

--- src/components/LocationSearch.jsx
+++ src/components/LocationSearch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import {
     Box,
     TextField,
@@ -9,16 +10,63 @@ import {
   import Autocomplete from "@mui/material/Autocomplete";
   import InputAdornment from "@mui/material/InputAdornment";
   import SearchIcon from "@mui/icons-material/Search";
+  import { countries, provinces} from "../data/LocationData";
 
+export default function LocationSearch() {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [selectedProvince, setSelectedProvince] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
 
-export default function LocationSearch({cities}) {
+  const handleCountryChange = (event, newCountry) => {
+    setSelectedCountry(newCountry);
+    setSelectedProvince(null);  
+    setSelectedCity(null);
+  };
+
+  const handleProvinceChange = (event, newProvince) => {
+    setSelectedProvince(newProvince);
+    setSelectedCity(null);
+  };
   return (
     <Box my={8}  >
-          <Autocomplete
+       <Autocomplete
+       id='country-search'
+        options={countries.map((country) => country.label)}
+        onChange={handleCountryChange}
+        renderInput={(params) => (
+          <TextField
+          sx={{'& ::placeholder':{fontFamily:'IranYekan'}}}
+           {...params}  placeholder="اول کشورتو انتخاب کن" />
+        )}
+      />
+    { selectedCountry && <Autocomplete
+      id='province-search'
+        options={selectedCountry ? countries.find(c => c.label === selectedCountry).provinces : []}
+        onChange={handleProvinceChange}
+        renderInput={(params) => (
+          <TextField
+          sx={{'& ::placeholder':{fontFamily:'IranYekan'}}}
+           {...params}  placeholder="حالا استانتو سرچ کن" />
+        )}
+        disabled={!selectedCountry}
+      />}
+         {selectedCountry && selectedProvince &&  <Autocomplete
+        options={selectedProvince ? provinces[selectedProvince] : []}
+        onChange={(event, newCity) => setSelectedCity(newCity)}
+        renderInput={(params) => (
+          <TextField 
+          sx={{'& ::placeholder':{fontFamily:'IranYekan'}}}
+          {...params}  placeholder="اینجاهم باید شهرتو انتخاب کنی" />
+        )}
+        disabled={!selectedProvince}
+      />}
+        </Box>
+  )
+}
+
+
+{/* <Autocomplete
           id='city-search'
-            PopperComponent={(props) => (
-              <Popper {...props} className="autocomplete-popper" />
-            )}
             PaperComponent={(props) => (
               <Paper {...props} className="autocomplete-listbox" />
             )}
@@ -45,7 +93,4 @@ export default function LocationSearch({cities}) {
                 }}
               />
             )}
-          />
-        </Box>
-  )
-}
+          /> */}
