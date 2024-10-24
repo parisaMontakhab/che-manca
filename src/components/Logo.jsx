f249935169c5e5a2992314f19b89482d0a0c1179
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-24 10:58:47 +0200

    refactor: changed index.css of image style and add logo classname and style

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ import { Box } from "@mui/material";
 export default function Logo() {
   return (
     <Box display="flex" justifyContent="center" alignItems="center" p={2}>
-      <img src={logo} alt="chemanca-logo" />
+      <img src={logo} alt="chemanca-logo" className="logo" />
     </Box>
   );
 }
