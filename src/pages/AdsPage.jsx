e0f5c35ceba9cb34600cb10b21c701f35ef87f76
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-28 13:11:12 +0100

    feat: add AppBar and toolBar and classname app-bar with style

--- src/pages/AdsPage.jsx
+++ src/pages/AdsPage.jsx
@@ -8,9 +8,18 @@ import { styled } from '@mui/material/styles';
 
 export default function AdsPage() {
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box >
      <header>
-      
+      <Box display='flex'>
+        <AppBar position='fixed' className='app-bar'>
+          <Toolbar>
+
+          </Toolbar>
+
+        </AppBar>
+
+      </Box>
+
      </header>
     </Box>
   )
