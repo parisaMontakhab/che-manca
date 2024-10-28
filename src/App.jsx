4bf5c671f93ed69389402415a2b5dd4da079347c
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-28 12:49:28 +0100

    feat: add AdsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles/App.css";
 import HomePage from "./pages/HomePage";
+import AdsPage from "./pages/AdsPage";
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <HomePage/>
+      <AdsPage/>
     </>
   );
 }
---
+++ src/pages/AdsPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Grid2, Paper, Drawer, List, ListItem, ListItemIcon, ListItemText, Box, Container } from '@mui/material';
+import { Menu as MenuIcon, Home as HomeIcon, Category as CategoryIcon } from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
+
+
+
+
+export default function AdsPage() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+     <header>
+      
+     </header>
+    </Box>
+  )
+}
