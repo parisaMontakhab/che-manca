ba66a15c4b712738f0605740e89c4999dda4cb2b
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-28 14:36:16 +0100

    refactor: changed name class of app-bar to ads-bar

--- src/pages/AdsPage.jsx
+++ src/pages/AdsPage.jsx
@@ -1,32 +1,50 @@
-import React from 'react';
+import React from "react";
 import logo from "../assets/images/logo/logo.png";
-import { AppBar, Toolbar, IconButton, Typography, Grid2, Paper, Drawer, List, ListItem, ListItemIcon, ListItemText, Box, Container, Divider } from '@mui/material';
-import { Menu as MenuIcon, Home as HomeIcon, Category as CategoryIcon } from '@mui/icons-material';
-import { styled } from '@mui/material/styles';
-
-
-
+import Button from "@mui/material/Button";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Grid2,
+  Paper,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Box,
+  Container,
+  Divider,
+  Link,
+} from "@mui/material";
+import {
+  Menu as MenuIcon,
+  Home as HomeIcon,
+  Category as CategoryIcon,
+} from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
 
 export default function AdsPage() {
   return (
-    <Box >
-     <header>
-      <Box display='flex'>
-        <AppBar position='fixed' className='app-bar' >
-          <Toolbar>
-            <Box>
-              <img src={logo} alt='logo image' className='adsPage-logo'/>
-            </Box>
-            
+    <Box>
+      <header>
+        <Box display="flex">
+          <AppBar position="fixed" className="ads-bar">
+            <Toolbar>
+              <img src={logo} alt="logo image" className="adsPage-logo" />
 
-
-          </Toolbar>
-
-        </AppBar>
-
-      </Box>
-
-     </header>
+              <Button className="" >
+                <LocationOnIcon />
+                <Link underline="none" >
+                <Typography >تهران</Typography>
+                </Link>
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </header>
     </Box>
-  )
+  );
 }
