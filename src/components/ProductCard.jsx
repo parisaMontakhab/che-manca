f6134ab86d95c111497c3cad1adb68e8f7810391
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-11-23 19:16:24 +0100

    refactor: add productCard__desc

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,59 +1,63 @@
 import React from "react";
-import { Box, Card, CardContent, CardMedia, Grid2, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid2,
+  Typography,
+} from "@mui/material";
 import ChatBubbleOutlineRoundedIcon from "@mui/icons-material/ChatBubbleOutlineRounded";
 import ShoesAdidas from "../assets/images/image/shoes.jpg";
 
 export default function ProductCard() {
   return (
     <Grid2>
-        <Card className="productCard__card ">
-      <Box component='div' className="productCard__mainBox">
-        <CardContent>
-          <Typography
-            component="div"
-          className="productCard__title "
+      <Card className="productCard__card ">
+        <Box component="div" className="productCard__mainBox">
+          <CardContent>
+            <Typography component="div" className="productCard__title ">
+              کفش کتونی اصل آدیداس
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              component="div"
+              className="productCard__desc"
+            >
+              در حد نو
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              component="div"
+              className="productCard__desc"
+            >
+              2،500 تومان
+            </Typography>
+          </CardContent>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-around",
+              marginBottom: 2,
+            }}
           >
-            کفش کتونی اصل آدیداس
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            component="div"
-            sx={{ color: "text.secondary", fontFamily: "IranYekan" }}
-          >
-            در حد نو
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            component="div"
-            sx={{ color: "text.secondary", fontFamily: "IranYekan" }}
-          >
-            2،500 تومان
-          </Typography>
-        </CardContent>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-around",
-            marginBottom: 2,
-          }}
-        >
-          <Typography
-            component="div"
-            sx={{ fontFamily: "IranYekan", fontSize: 12, color: "gray" }}
-          >
-            لحظاتی پیش در اقدسیه
-          </Typography>
+            <Typography
+              component="div"
+              sx={{ fontFamily: "IranYekan", fontSize: 12, color: "gray" }}
+            >
+              لحظاتی پیش در اقدسیه
+            </Typography>
 
-          <ChatBubbleOutlineRoundedIcon sx={{ color: "gray" }} />
+            <ChatBubbleOutlineRoundedIcon sx={{ color: "gray" }} />
+          </Box>
         </Box>
-      </Box>
-      <CardMedia
-        component="img"
-        image={ShoesAdidas}
-        sx={{ width: 150, padding: 1.5, borderRadius: "16px" }}
-      />
-    </Card>
+        <CardMedia
+          component="img"
+          image={ShoesAdidas}
+          sx={{ width: 150, padding: 1.5, borderRadius: "16px" }}
+        />
+      </Card>
     </Grid2>
   );
 }
