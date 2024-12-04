6e6af21e0b45969084a54f2f3050104e9f88dc41
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-12-04 10:33:43 +0100

    pritier

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools/>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>
 );
