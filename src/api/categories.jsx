9cdab0a67499a4e8b88e7416a137381261dfa363
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-12-04 12:42:29 +0100

    pritier

--- src/api/categories.jsx
+++ src/api/categories.jsx
@@ -1,18 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+//categories//
+const fetchCategories = async () => {
+  const { data } = await axios.get(
+    "https://client.mobile.chemanca.com/api/advertisements/AdsCategory/GetAllAdvertisementCategories"
+  );
 
+  return data;
+};
 
-const fetchCategories = async ()=>{
-    const {data} = await axios.get('https://client.mobile.chemanca.com/api/advertisements/AdsCategory/GetAllAdvertisementCategories');
-    
-    
-    return data;
-}
-
-export const useCategories = ()=>{
-    return useQuery({
-        queryKey:['categories'],
-        queryFn:fetchCategories,
-    })
-}
+export const useCategories = () => {
+  return useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+};
