a7817124a3213bd02ec670d179c5d6e67c982573
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-12-16 16:17:36 +0100

    feat: add note in stringcomponent

--- src/components/StringComponent.jsx
+++ src/components/StringComponent.jsx
@@ -1,109 +1,105 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import {
-    ListItem,
-    ListItemText,
-    Button,
-    Collapse,
-    Box,
-    Typography,
-    Autocomplete,
-    TextField,
-    Divider,
-    Paper,
-  } from "@mui/material";
-  import ExpandLess from "@mui/icons-material/ExpandLess";
-  import ExpandMore from "@mui/icons-material/ExpandMore";
-  import FiberManualRecordRoundedIcon from "@mui/icons-material/FiberManualRecordRounded";
+  ListItem,
+  ListItemText,
+  Button,
+  Collapse,
+  Box,
+  Typography,
+  Autocomplete,
+  TextField,
+  Divider,
+  Paper,
+} from "@mui/material";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import FiberManualRecordRoundedIcon from "@mui/icons-material/FiberManualRecordRounded";
 
-export default function StringComponent (  {title,
-    options,
-    openItems,
-    handleClick}) {
-    const [value, setValue] = useState([]);
-    const [inputValue, setInputValue] = useState("");
-    const [idesList,setIdesList] = useState(null);
-    
-  
-  
-    const handleDeletValue = ()=>{
-      setValue([]);
-      setInputValue('');
-    }
+export default function StringComponent({
+  title,
+  options,
+  openItems,
+  handleClick,
+}) {
+  const [value, setValue] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [idesList, setIdesList] = useState(null);
+  //idesList va setIdesList dorost shode baraye inke vaghti user roye optionhaye autocompolete click mikone va string daryaft mikone ma id ono doashte bashim baraye marahele bad dar ba database lazem mishod//
 
-    const handleSelectedId = (newInputValue)=>{
-        options?.map((option)=>{
-            if(option.text == newInputValue){
-                setIdesList(option.value)
-            }
-        })
-    }
-    
-  
-    return (
-      <div>
-        <ListItem
-          onClick={() => handleClick(title)}
-          className="ads-sideBar__listItem "
-        >
-          {openItems[title] ? (
-            <ExpandLess className="ads-sideBar__itemIcon" />
+  const handleDeletValue = () => {
+    setValue([]);
+    setInputValue("");
+  };
+
+  const handleSelectedId = (newInputValue) => {
+    options?.map((option) => {
+      if (option.text == newInputValue) {
+        setIdesList(option.value);
+      }
+    });
+  };
+
+  return (
+    <div>
+      <ListItem
+        onClick={() => handleClick(title)}
+        className="ads-sideBar__listItem "
+      >
+        {openItems[title] ? (
+          <ExpandLess className="ads-sideBar__itemIcon" />
+        ) : (
+          <ExpandMore className="ads-sideBar__itemIcon" />
+        )}
+        <ListItemText
+          primary={title}
+          disableTypography
+          className="ads-sideBar__collapsText"
+        />
+        {inputValue.trim() !== "" &&
+          (openItems[title] ? (
+            <Button
+              onClick={handleDeletValue}
+              className="ads-sideBar__deletBtn"
+            >
+              حذف
+            </Button>
           ) : (
-            <ExpandMore className="ads-sideBar__itemIcon" />
-          )}
-          <ListItemText
-            primary={title}
-            disableTypography
-            className="ads-sideBar__collapsText"
+            <FiberManualRecordRoundedIcon className="ads-sideBar__deletIcon" />
+          ))}
+      </ListItem>
+
+      <Collapse in={openItems[title]} timeout="auto" unmountOnExit>
+        <Box className="ads-sideBar__priceBox">
+          <Autocomplete
+            fullWidth
+            PaperComponent={(props) => (
+              <Paper {...props} className="ads-sideBar__pricePaper" />
+            )}
+            size="small"
+            className="ads-sideBar__priceAutocomplete"
+            options={options?.map((option) => option.text) || []}
+            getOptionLabel={(option) => String(option)}
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+
+              handleSelectedId(newInputValue);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                placeholder=" لطفا یه مورد انتخاب کن"
+                className="ads-sideBar__priceSpan"
+              />
+            )}
           />
-          {inputValue.trim() !== "" &&
-            (openItems[title] ? (
-              <Button
-                onClick={handleDeletValue}
-                className="ads-sideBar__deletBtn"
-              >
-                حذف
-              </Button>
-            ) : (
-              <FiberManualRecordRoundedIcon className="ads-sideBar__deletIcon" />
-            ))}
-        </ListItem>
-  
-        <Collapse in={openItems[title]} timeout="auto" unmountOnExit>
-          <Box className="ads-sideBar__priceBox">
-            <Autocomplete
-              fullWidth
-              PaperComponent={(props) => (
-                <Paper {...props} className="ads-sideBar__pricePaper" />
-              )}
-              size="small"
-              className="ads-sideBar__priceAutocomplete"
-              options={options?.map((option)=>option.text) || []}
-              getOptionLabel={(option) => String(option)}
-              value={value}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-                
-               
-                
-              }}
-              inputValue={inputValue}
-              onInputChange={(event, newInputValue) => {
-                setInputValue(newInputValue);
-               
-                handleSelectedId(newInputValue);
-              }}
-              
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  placeholder=" لطفا یه مورد انتخاب کن"
-                  className="ads-sideBar__priceSpan"
-                />
-              )}
-            />
-          </Box>
-        </Collapse>
-        <Divider sx={{ width: "80%", marginTop: 2 }} />
-      </div>
-    );
+        </Box>
+      </Collapse>
+      <Divider sx={{ width: "80%", marginTop: 2 }} />
+    </div>
+  );
 }
